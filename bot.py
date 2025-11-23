1f232d1b1d35b3d23a81f6a4cf2152fa57ee4a62
EllYUSS <elvin.macos@gmail.com>
2025-11-23 22:16:58 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,39 +1,25 @@
-import os
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher
 from aiogram.utils.executor import start_webhook
+import os
 
-API_TOKEN = os.getenv("BOT_TOKEN")  # токен бота из переменных окружения
+TOKEN = "8452605972:AAH32IFCrVYG-lvmNhm3zsjQ-I_Hxqzkwpg"
 
-WEBHOOK_HOST = f"https://{os.getenv('RAILWAY_STATIC_URL')}"
-WEBHOOK_PATH = f"/webhook/{API_TOKEN}"
-WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
-
-WEBAPP_HOST = "0.0.0.0"
-WEBAPP_PORT = int(os.getenv("PORT", 8000))
-
-bot = Bot(token=API_TOKEN)
+bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
-@dp.message_handler()
-async def echo(message: types.Message):
-    await message.answer(f"Ты написал: {message.text}")
+WEBHOOK_PATH = "/webhook"
+WEBHOOK_URL = f"https://worker-production-abb5.up.railway.app{WEBHOOK_PATH}"
+WEBAPP_HOST = "0.0.0.0"
+WEBAPP_PORT = int(os.environ.get("PORT", 8000))  # Railway автоматически даёт порт
 
-async def on_startup(dp):
-    await bot.delete_webhook()
+async def on_startup(dispatcher):
     await bot.set_webhook(WEBHOOK_URL)
     print(f"Webhook установлен: {WEBHOOK_URL}")
 
-async def on_shutdown(dp):
-    await bot.delete_webhook()
-    print("Webhook удален")
-
-if __name__ == "__main__":
-    start_webhook(
-        dispatcher=dp,
-        webhook_path=WEBHOOK_PATH,
-        on_startup=on_startup,
-        on_shutdown=on_shutdown,
-        skip_updates=True,
-        host=WEBAPP_HOST,
-        port=WEBAPP_PORT,
-    )
+start_webhook(
+    dispatcher=dp,
+    webhook_path=WEBHOOK_PATH,
+    on_startup=on_startup,
+    host=WEBAPP_HOST,
+    port=WEBAPP_PORT,
+)
