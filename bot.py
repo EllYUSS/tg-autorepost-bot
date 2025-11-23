340b8225a83191a44aab9592035bfb6d6134ce0a
EllYUSS <elvin.macos@gmail.com>
2025-11-23 23:50:57 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,6 @@
 from aiogram import Bot, Dispatcher, types
-from aiogram.types import InputFile
 from aiogram.utils import executor
 import logging
-import os
 
 API_TOKEN = '8452605972:AAH32IFCrVYG-lvmNhm3zsjQ-I_Hxqzkwpg'
 SOURCE_CHANNEL = -1002913212827
@@ -10,31 +8,23 @@ TARGET_CHANNEL = -1003248459795
 
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
-dp = Dispatcher()
+dp = Dispatcher(bot)
 
 @dp.channel_post_handler()
 async def repost_message(message: types.Message):
     try:
-        # Текст
         if message.text:
             await bot.send_message(TARGET_CHANNEL, message.text)
-        
-        # Фото
         elif message.photo:
-            photo = message.photo[-1]  # Выбираем максимальное качество
+            photo = message.photo[-1]
             caption = message.caption or ""
             await bot.send_photo(TARGET_CHANNEL, photo.file_id, caption=caption)
-        
-        # Видео
         elif message.video:
             caption = message.caption or ""
             await bot.send_video(TARGET_CHANNEL, message.video.file_id, caption=caption)
-        
-        # Документы (pdf, docx и т.д.)
         elif message.document:
             caption = message.caption or ""
             await bot.send_document(TARGET_CHANNEL, message.document.file_id, caption=caption)
-        
         else:
             logging.info("Неизвестный тип сообщения, пропускаем")
     except Exception as e:
